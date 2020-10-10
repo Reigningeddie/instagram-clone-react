896b75a144da43507e57ba8f14f6a4e4f4aadaf3
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-10-10 00:25:58 -0700

    added logo and changed background off-white

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <h1></h1>
+      <div className="app__header">
+        <img 
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      {/* Posts */}
+      {/* Posts */}
     </div>
   );
 }
