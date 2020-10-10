62cc4121a25768ba0795797126bde96f03e269ec
Eduardo Aguilar <eaguilar92105@yahoo.com>
2020-10-10 01:03:42 -0700

    created post component and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Post from './Post'; 
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+
       <div className="app__header">
         <img 
           className="app__headerImage"
@@ -12,7 +13,9 @@ function App() {
           alt=""
         />
       </div>
+      <h1>lets build an instagram clone with react 🚀!</h1>
 
+      <Post />
       {/* Posts */}
       {/* Posts */}
     </div>
